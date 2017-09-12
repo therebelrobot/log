ee8be8f6fa2478f7af015390684f8ede8775e117
Ozzy Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2017-09-12 10:39:22 -0700

    added option for logdns logging

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+const ldna = require('logdna')
 const winston = require('winston')
 const ecs = require('ecs-logs-js')
 
-module.exports = function log({ name = 'log', level = 'info', env = 'production' }) {
-  if (env !== 'development') return new winston.Logger({
-    level,
-    levels: winston.config.syslog.levels,
-    transports: [ new ecs.Transport({ level, name }) ]
-  })
-  const logger = new winston.Logger({
+module.exports = function log({ name = 'log', level = 'info', env = 'production', logdna }) {
+  let logger
+  if (env !== 'development') {
+    logger = new winston.Logger({
+      level,
+      levels: winston.config.syslog.levels,
+      transports: [
+        new ecs.Transport({ level, name })
+      ]
+    })
+    if (logdna) {
+      logdna = Object.assign({}, {
+        index_meta: true, // Defaults to false, when true ensures meta object will be searchable
+        handleExceptions: true // Only add this line in order to track exceptions
+      }, logdna)
+      logger.add(winston.transports.Logdna, logdna)
+    }
+    return logger
+  }
+  logger = new winston.Logger({
     level,
     levels: winston.config.syslog.levels
   })
